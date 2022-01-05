fe81006b77e617e21bbbb707d70db8ca3fa2731c
Pogajoa <87626122+Pogajoa@users.noreply.github.com>
2022-01-06 07:07:21 +0900

    Json파일 추가

---
+++ Assets/PlayerController1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public PlayerData playerData;
+}
+  public class PlayerData{
+      public string name;
+      public int age;
+      public int level;
+      public bool isDead;
+      public string[] items;
+  }
+
